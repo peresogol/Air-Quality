cdc80454d4b57823a3dae9e4ee55fe087ca209b1
Jonathan Bonnefoy <jonathan.bonnefoy@outlook.com>
2021-03-15 18:50:27 +0100

    Created app logo, database for each place (=cities which contain an id, name, longitude, latitude and precise if it's a favourite one). Created methods in order to view and insert into database. Modified attributes of PersonalizedListData class (split GPS coordinates into longitude and latitude)

--- app/src/main/java/com/example/qualitair/FavouriteCities.java
+++ app/src/main/java/com/example/qualitair/FavouriteCities.java
@@ -1,5 +1,6 @@
 package com.example.qualitair;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,16 +16,15 @@ public class FavouriteCities extends AppCompatActivity {
     private ListView listView;
     private List<PersonalizedListData> listData;
     private PersonalizedListAdapter adapter;
+    private SQLClient db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history);
+        this.db = new SQLClient(this);
+        this.viewData();
 
-        this.listData = getListData();
-        this.listView = (ListView)findViewById(R.id.listView);
-        this.adapter = new PersonalizedListAdapter(this.listData, this);
-        this.listView.setAdapter(adapter);
         this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -37,16 +37,37 @@ public class FavouriteCities extends AppCompatActivity {
                 } else {
                     Toast.makeText(FavouriteCities.this,"" + data.getPlaceName() + " a été enlevé des favoris", Toast.LENGTH_SHORT).show();
                 }
-
             }
         });
     }
 
+    private void viewData() {
+        Cursor cursor = db.viewData();
+        this.listData = this.getListData();
+        // if there is at least one result
+        if (cursor.moveToFirst()) {
+            do {
+                String name = cursor.getString(1);
+                String longitude  = cursor.getString(2);
+                String latitude = cursor.getString(3);
+                boolean isFavourite = (cursor.getInt(4) == 1);
+                PersonalizedListData data = new PersonalizedListData(name,longitude,latitude,isFavourite);
+                this.listData.add(data);
+                // while there is another next result
+            } while (cursor.moveToNext());
+        } else {
+            Toast.makeText(this,"Il n'y a pas de valeurs à lire",Toast.LENGTH_SHORT).show();
+        }
+        this.listView = (ListView)findViewById(R.id.listView);
+        this.adapter = new PersonalizedListAdapter(this.listData, this);
+        this.listView.setAdapter(this.adapter);
+    }
+
     private List<PersonalizedListData> getListData() {
         List<PersonalizedListData> listData = new ArrayList<>();
-        PersonalizedListData data1 = new PersonalizedListData("Toulouse", "44.44, 44.44", true);
-        PersonalizedListData data2 = new PersonalizedListData("Cahors", "33.33, 33.33", true);
-        PersonalizedListData data3 = new PersonalizedListData("Vierzon", "22.22, 22.22", false);
+        PersonalizedListData data1 = new PersonalizedListData("Toulouse", "44.44", "44.44", true);
+        PersonalizedListData data2 = new PersonalizedListData("Cahors", "33.33", "33.33", true);
+        PersonalizedListData data3 = new PersonalizedListData("Vierzon", "22.22", "22.22", false);
         listData.add(data1);
         listData.add(data2);
         listData.add(data3);
