b0c39b9a690a4e74de1e1decb6c66ced0c13e321
VassiliKan <sachacauli@gmail.com>
2021-03-17 14:27:18 +0100

    create landscape variation for weather and pollution display result

--- app/src/main/java/com/example/qualitair/CallAPI.java
+++ app/src/main/java/com/example/qualitair/CallAPI.java
@@ -177,7 +177,7 @@ public class CallAPI extends AppCompatActivity {
 
     private void getLocation() {
         Log.v("oaa", "check0");
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
