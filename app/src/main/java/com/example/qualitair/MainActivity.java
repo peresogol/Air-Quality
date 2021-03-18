ad5403b379da69140d1cb55c2729f91bae709365
VassiliKan <sachacauli@gmail.com>
2021-03-18 11:21:12 +0100

    final version of first sprint

--- app/src/main/java/com/example/qualitair/MainActivity.java
+++ app/src/main/java/com/example/qualitair/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
     private Place placeResult;
     private TextView nearestCity;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
